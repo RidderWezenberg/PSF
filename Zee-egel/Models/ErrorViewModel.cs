91986ef36bbe46fc21a1bbad05ca74f9c6a7f7f0
Alexander Wezenberg <a.wezenberg@ridder.nl>
2019-10-31 19:24:22 +0100

    Solution with home page

---
+++ Zee-egel/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Zee_egel.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
