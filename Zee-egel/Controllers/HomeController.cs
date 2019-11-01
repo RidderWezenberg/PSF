db3da670c3b9c6ad1f21b89851e3cc2c58a58561
Alexander Wezenberg <a.wezenberg@ridder.nl>
2019-11-01 14:30:23 +0100

    Hernoemen actions

--- Zee-egel/Controllers/HomeController.cs
+++ Zee-egel/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Zee_egel.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Gallery()
         {
             return View();
         }
